ea0a6bbf19070c3f48836cd4035dccce999bbcc3
Pavel Linkesch <pavel.linkesch@gmail.com>
2016-09-06 17:36:37 +0200

    Fix unknown props warning on React v15 (#7)
    * fix unknown props warning
    
    * use babel-preset-stage-2

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
 				loader: 'babel',
 				exclude: /node_modules/,
 				query: {
-					presets: ['react', 'es2015']
+					presets: ['react', 'es2015', 'stage-2']
 				}
 			}
 		]
